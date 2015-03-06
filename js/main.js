9ef9f31ada210fcc9b3226f5b0cf322f09eb061b
张琪锋 <sealzqf@gmail.com>
2015-03-06 17:59:19 +0800

    add and update

--- js/main.js
+++ js/main.js
@@ -108,30 +108,3 @@ DS.member.work = {
 		});
 	}
 };
-
-//新闻
-DS.news = {};
-DS.news.index = {
-	init:function(){
-		this.initCarousel();
-		this.initTab();
-	},
-	initCarousel:function(){
-		$("#topNewsCarousel").owlCarousel({
-			autoPlay: true,
-			navigation : true,
-			navigationText : ['&lt;',"&gt;"],
-			slideSpeed : 300,
-			paginationSpeed : 400,
-			paginationNumbers : false,
-			singleItem:true
-		});
-	},
-	initTab:function(){
-		DS.widget("Tab",{
-			tabBox:"#hotnewsTabBox",
-			tabBtn:".tab",
-			tabBoxClass:".hotnews-list"
-		});
-	}
-}
